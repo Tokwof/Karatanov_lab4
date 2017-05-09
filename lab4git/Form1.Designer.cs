c5ff444ad8dc56758a3679a158b43e557a08a88b
Tatiana Marchenko <tokwof@gmail.com>
2017-05-09 23:48:18 +0300

    добавлена кнопка в ветке

--- lab4git/Form1.Designer.cs
+++ lab4git/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +42,21 @@
             this.label1.Text = "Марченко, лаб4 (гит)";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "сделано в ветке";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
